2aa2f17db155330f4369345e7946b148f298731a
Guilherme Desidério <guilhermedesiderio23@gmail.com>
2022-12-15 21:59:04 -0300

    Create contacts

--- app.py
+++ app.py
@@ -5,4 +5,8 @@ minha_agenda = open(nome_arquivo, 'w')
 print(f'Arquivo {nome_arquivo} criado com sucesso!')
 minha_agenda.close()
 
-#
+# Cria 3 contatos com a estrutura (id_contato, nome_contato)
+contato1 = ('1', 'Maria Joaquina de Amaral Pereira Goes')
+contato2 = ('2', 'Carlos de Andrade')
+contato3 = ('3', 'Antônio da Silva')
+
